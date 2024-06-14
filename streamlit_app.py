041164b9b0ed4dbf73c77fce23c2a5b37ec72ce4
Wenxin Zhang <vincentzhang666ca@gmail.com>
2024-06-14 16:02:41 -0700

    add all files

---
+++ streamlit_app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import random
+import time
+from openai import OpenAI
+
+def response_generator():
+    response = random.choice(
+        [
+            "Hi there, how can i help?",
+            "Rock n roll!",
+            "You wanna a piece of me boy?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+st.title("Chat Bot Example")
+
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key = st.secrets["OPENAI_API_KEY"])
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["chat_history"])
+
+if user_prompt := st.chat_input("Say something here"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({
+        "role": "user",
+        "chat_history": user_prompt
+    })
+
+    # bot_response = "Echoing: {}".format(user_prompt)
+    # Display bot response in chat message container
+    with st.chat_message("assistant"):
+        # st.markdown(bot_response)
+        bot_response = st.write_stream(response_generator())
+        # stream = client.chat.completions.create(
+        #     model = st.session_state["openai_model"],
+        #     messages=[
+        #         {"role": m["role"], "chat_history": m["chat_history"]} for m in st.session_state.messages
+        #     ],
+        #     stream = True
+        # )
+        # bot_response = st.write_stream(stream)
+    # Add assitant response to chat history
+    st.session_state.messages.append({
+        "role": "assistant",
+        "chat_history": bot_response
+    })
